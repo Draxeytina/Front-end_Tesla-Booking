0bff3687ef5e7419154e7aa6fa9f5fc77360398c
Tinashe Murambinda <tmurambinda01@gmail.com>
2022-10-25 13:57:08 +0200

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,27 +1,28 @@
-import logo from './logo.svg';
+import React from 'react';
+import {
+  BrowserRouter as Router, Routes, Route, Navigate,
+} from 'react-router-dom';
+import Navbar from './components/navbar/Navbar';
+import Reservations from './components/myReservations/Reservations';
 import './App.css';
+import './App.scss';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit
-          {' '}
-          <code>src/App.js</code>
-          {' '}
-          and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Router>
+        <Navbar />
+        <Routes>
+          <>
+            {/* <Route path="/cars" element={<Main />} /> */}
+            {/* <Route path="/reservations/create" element={<Reserve />} /> */}
+            <Route path="/users/:id/reserved_cars" element={<Reservations />} />
+            {/* <Route path="/cars/create" element={<New />} /> */}
+            {/* <Route path="/cars/:id" element={<Delete />} /> */}
+            <Route path="/*" element={<Navigate to="/" replace />} />
+          </>
+        </Routes>
+      </Router>
     </div>
   );
 }
