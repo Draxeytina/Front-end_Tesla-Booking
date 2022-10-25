0a417065dd52d166eaf8561ff942164847a32543
Tinashe Murambinda <tmurambinda01@gmail.com>
2022-10-25 20:14:53 +0200

    Update app.js

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import {
-  BrowserRouter as Router, Routes, Route, Navigate,
+  BrowserRouter as Router, Routes, Route,
 } from 'react-router-dom';
 import Navbar from './components/navbar/Navbar';
 import Reservations from './components/myReservations/Reservations';
@@ -19,7 +19,7 @@ function App() {
             <Route path="/users/:id/reserved_cars" element={<Reservations />} />
             {/* <Route path="/cars/create" element={<New />} /> */}
             {/* <Route path="/cars/:id" element={<Delete />} /> */}
-            <Route path="/*" element={<Navigate to="/" replace />} />
+            {/* <Route path="/*" element={<Navigate to="/" replace />} /> */}
           </>
         </Routes>
       </Router>
