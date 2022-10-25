76d2b171192658a8ed4d804f9598e133ba5e70fb
franclobo <fjbl2788@gmail.com>
2022-10-25 16:58:01 -0500

    create add form

--- src/components/Adds/Adds.js
+++ src/components/Adds/Adds.js
@@ -1,73 +1,105 @@
-import { useSelector, useDispatch } from 'react-redux';
-import { FetchCars, AddCar } from '../../redux/Adds/adds';
-import TextField from '@material-ui/core/TextField';
+import TextField from '@mui/material/TextField';
+import Button from '@mui/material/Button';
 
 export default function Adds() {
-  const dispatch = useDispatch();
-  const cars = useSelector((state) => state.cars);
-
-  const handleSubmit = (e) => {
+  const handleSubmit = async (e) => {
+    console.log("I'm here");
     e.preventDefault();
-    dispatch(FetchCars());
+    const response = await fetch('http://127.0.0.1:3000/cars/create', {
+      method: 'POST',
+      headers: { 'Content-Type': 'application/json' },
+      body: JSON.stringify({
+        model: e.target.model.value,
+        color: e.target.color.value,
+        image: e.target.image.value,
+        range: e.target.range.value,
+        motor: e.target.motor_type.value,
+        acceleration: e.target.acceleration_time.value,
+        price: e.target.booking_price.value,
+        duration: e.target.booking_duration.value,
+      }),
+    });
+    console.log('response', response);
   };
 
   return (
     <div>
       <form
-      onSubmit={handleSubmit}
-      id="form"
-      autoComplete="off"
+        id="form"
+        autoComplete="off"
+        onSubmit={handleSubmit}
       >
         <TextField
           id="standard-basic"
-          label="Standard"
+          label="Model"
+          variant="outlined"
           type="text"
           name="model"
           placeholder="Model"
         />
         <TextField
           id="standard-basic"
-          label="Standard"
+          label="Color"
+          variant="outlined"
           type="text"
           name="color"
           placeholder="Color"
         />
         <TextField
           id="standard-basic"
-          label="Standard"
+          label="Image"
+          variant="outlined"
+          type="text"
+          name="image"
+          placeholder="https://"
+        />
+        <TextField
+          id="standard-basic"
+          label="Range"
+          variant="outlined"
           type="text"
           name="range"
           placeholder="Range"
         />
         <TextField
           id="standard-basic"
-          label="Standard"
+          label="Motor"
+          variant="outlined"
           type="text"
-          name="motor"
+          name="motor_type"
           placeholder="Motor"
         />
         <TextField
           id="standard-basic"
-          label="Standard"
+          label="Acceleration"
+          variant="outlined"
           type="text"
-          name="acceleration"
+          name="acceleration_time"
           placeholder="Acceleration"
         />
         <TextField
           id="standard-basic"
-          label="Standard"
+          label="Price"
+          variant="outlined"
           type="text"
-          name="price"
+          name="booking_price"
           placeholder="Price"
         />
         <TextField
           id="standard-basic"
-          label="Standard"
+          label="Duration"
+          variant="outlined"
           type="text"
-          name="duration"
+          name="booking_duration"
           placeholder="Duration"
         />
-        <button type="submit">Submit</button>
+        <TextField
+          id="outlined-multiline-static"
+          label="Description"
+          multiline
+          rows={4}
+        />
+        <Button type="submit" variant="contained" color="primary">Submit</Button>
       </form>
     </div>
   );
