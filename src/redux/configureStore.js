1425592783ab695401e91032512a01cd5b75f700
Fabien <fabienswift@gmail.com>
2022-10-26 16:59:13 -0400

    Add details to configure store

--- src/redux/configureStore.js
+++ src/redux/configureStore.js
@@ -1,9 +1,11 @@
 import { configureStore } from '@reduxjs/toolkit';
+import detailsReducer from './cars/car-details';
 import carsReducer from './cars/cars';
 
 const store = configureStore({
   reducer: {
     cars: carsReducer,
+    details: detailsReducer,
   },
 });
 
