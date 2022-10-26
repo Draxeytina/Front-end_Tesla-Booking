d8fbbf858ad96f0aa0606aa62dedb5231293d93b
Francisco Borja Lobato <fjbl2788@gmail.com>
2022-10-25 21:29:43 -0500

    Revert "Add page"

--- src/App.js
+++ src/App.js
@@ -6,7 +6,6 @@ import Navbar from './components/navbar/Navbar';
 import Reservations from './components/myReservations/Reservations';
 import ReserveForm from './components/Reserve Form/ReserveForm';
 import Home from './components/Home';
-import Adds from './components/Adds/Adds';
 import './App.css';
 import './App.scss';
 
@@ -18,7 +17,6 @@ function App() {
         <Routes>
           <>
             <Route exact path="/" element={<Home />} />
-            <Route exact path="/cars/create" element={<Adds />} />
             {/* <Route path="/reservations/create" element={<Reserve />} /> */}
             <Route path="/users/:id/reserved_cars" element={<Reservations />} />
             {/* <Route path="/cars/create" element={<New />} /> */}
--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import { BrowserRouter } from 'react-router-dom';
 import { Provider } from 'react-redux';
 import store from './redux/configureStore';
 import App from './App';
@@ -7,9 +8,11 @@ import App from './App';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <Provider store={store}>
-    <App />
-  </Provider>,
+  <BrowserRouter>
+    <Provider store={store}>
+      <App />
+    </Provider>
+  </BrowserRouter>,
 );
 
 // If you want to start measuring performance in your app, pass a function
