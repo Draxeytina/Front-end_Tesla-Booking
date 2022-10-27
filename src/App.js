bbd8fb59a7dc1f9561a92de8e09c1cc51336892f
Fabien <fabienswift@gmail.com>
2022-10-27 16:15:13 -0400

    Removed app styles

--- src/App.js
+++ src/App.js
@@ -6,8 +6,8 @@ import Navbar from './components/navbar/Navbar';
 import Reservations from './components/myReservations/Reservations';
 import Home from './components/Home';
 import Adds from './components/Adds/Adds';
-import './App.css';
-import './App.scss';
+// import './App.css';
+// import './App.scss';
 import Details from './components/Tesla-details';
 
 function App() {
@@ -15,17 +15,19 @@ function App() {
     <div className="App">
       <Router>
         <Navbar />
-        <Routes>
-          <>
-            <Route exact path="/" element={<Home />} />
-            <Route exact path="/cars/create" element={<Adds />} />
-            {/* <Route path="/reserve" element={<ReserveForm />} /> */}
-            <Route path="/users/:id/reserved_cars" element={<Reservations />} />
-            {/* <Route path="/cars/create" element={<New />} /> */}
-            <Route exact path="/cars/:id" element={<Details />} />
-            {/* <Route path="/*" element={<Navigate to="/" replace />} /> */}
-          </>
-        </Routes>
+        <div>
+          <Routes>
+            <>
+              <Route exact path="/" element={<Home />} />
+              <Route exact path="/cars/create" element={<Adds />} />
+              {/* <Route path="/reserve" element={<ReserveForm />} /> */}
+              <Route path="/users/:id/reserved_cars" element={<Reservations />} />
+              {/* <Route path="/cars/create" element={<New />} /> */}
+              <Route exact path="/cars/:id" element={<Details />} />
+              {/* <Route path="/*" element={<Navigate to="/" replace />} /> */}
+            </>
+          </Routes>
+        </div>
       </Router>
     </div>
   );
