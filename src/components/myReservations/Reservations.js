f52859132be1e5963cf67d478dc7f1deecb067e2
Tinashe Murambinda <tmurambinda01@gmail.com>
2022-11-01 21:18:31 +0200

    Update reservations for empty array

--- src/components/myReservations/Reservations.js
+++ src/components/myReservations/Reservations.js
@@ -25,26 +25,35 @@ const Reservations = (props) => {
   }, [reservations.length, dispatch]);
 
   const userReservations = reservations.filter((res) => res.userId === user.id ?? 1);
+  console.log(userReservations);
 
   return (
     <section className="my-reservations-wrap">
       <h2>RESERVATIONS</h2>
-      <div className="res-header">
-        <h5>Model</h5>
-        <h5>Reservation Date</h5>
-      </div>
-      <div className="res-body">
-        {userReservations.map((res) => (
-          <ul key={res.id}>
-            <Reservation
-              model={cars.find((car) => car.id === res.carId).model}
-              bookingDate={new Date(res.bookingDate).toDateString()}
-              id={res.id}
-              key={res.id}
-            />
-          </ul>
-        ))}
-      </div>
+      { userReservations.length === 0 ? (
+        <div className="res-header padding">
+          <h5>No reservations available. Kindly add a reservation.</h5>
+        </div>
+      ) : (
+        <>
+          <div className="res-header">
+            <h5>Model</h5>
+            <h5>Reservation Date</h5>
+          </div>
+          <div className="res-body">
+            {userReservations.map((res) => (
+              <ul key={res.id}>
+                <Reservation
+                  model={cars.find((car) => car.id === res.carId).model}
+                  bookingDate={new Date(res.bookingDate).toDateString()}
+                  id={res.id}
+                  key={res.id}
+                />
+              </ul>
+            ))}
+          </div>
+        </>
+      )}
     </section>
   );
 };
